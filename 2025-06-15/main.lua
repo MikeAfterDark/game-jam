d6052bcab4f89f647b9e4249ba40e745f3056b7c
Mikey <mike.gusak@gmail.com>
2025-06-14 18:44:35 -0500

    init

---
+++ 2025-06-15/main.lua
@@ -0,0 +1,3 @@
+function love.draw()
+    love.graphics.print("Hello Nerd", 500, 500)
+end
---
+++ cicd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# 1. Get folder name
+# 2. Build, use folder/build for config
+# 3. Upload, Check for itch upload config
