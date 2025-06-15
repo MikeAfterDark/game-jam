4579b25f4498a4462c51abb558fda5ef3ad7fe45
MikeAfterDark <mike.gusak@gmail.com>
2025-06-15 01:41:37 -0500

    setting up makelove

--- cicd.sh
+++ cicd.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# 1. Get folder name
+DIRECTORY=$1
+
+if [ ! -d "$DIRECTORY" ]; then
+    echo "Directory '$DIRECTORY' doesn't exist."
+    exit 1
+fi
+
 # 2. Build, use folder/build for config
+echo "Building project: $DIRECTORY"
+
 # 3. Upload, Check for itch upload config
